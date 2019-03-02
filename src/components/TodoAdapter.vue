cd5f20e69f9576b9f9f3447971f61e34ab4360bf
Eugene <shakuskys@gmail.com>
2019-03-02 09:52:03 +0300

    todos actions

--- src/components/TodoAdapter.vue
+++ src/components/TodoAdapter.vue
@@ -2,72 +2,45 @@
   <div class="todo-adapter">
     <TodoAdd @add-todo="addTodo" />
     <TodoList
-      :todos="niceTodos"
+      :todos="todos"
       @del-todo="deleteTodo"
     />
-    <button @click="mutate">
-      mutate
-    </button>
   </div>
 </template>
 
 <script>
-// import axios from 'axios';
+import { mapActions, mapGetters } from 'vuex';
 
 import TodoAdd from './TodoAdd';
 import TodoList from './TodoList';
 
 export default {
   name: 'TodoAdapter',
+  
   components: {
     TodoAdd,
     TodoList,
   },
+  
   computed: {
     todos() {
       return this.$store.state.todos;
     },
-    niceTodos() {
-      return this.$store.getters.niceTodos;
-    },
+    ...mapGetters([
+      'niceTodos',
+    ]),
   },
-  // data() {
-  //   return {
-  //     todos: [],
-  //   };
-  // },
-  // created() {
-  //   axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
-  //     .then((res) => {
-  //       this.todos = res.data;
-  //     })
-  //     .catch(err => console.error(err)); // eslint-disable-line no-console
-  // },
+  
+  created() {
+    this.$store.dispatch('getTodos');
+  },
+  
   methods: {
-    mutate() {
-      this.$store.commit('mutate');
-    },
+    ...mapActions([
+      'addTodo',
+      'deleteTodo',
+    ]),
   },
-  //   deleteTodo(id) {
-  //     axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
-  //       .then(() => {
-  //         this.todos = this.todos.filter(todo => todo.id !== id);
-  //       })
-  //       .catch(err => console.error(err)); // eslint-disable-line no-console
-  //   },
-  //   addTodo(newTodo) {
-  //     const { title, completed } = newTodo;
-      
-  //     axios.post('https://jsonplaceholder.typicode.com/todos', {
-  //       title,
-  //       completed,
-  //     })
-  //       .then((res) => {
-  //         this.todos = [...this.todos, res.data];
-  //       })
-  //       .catch(err => console.error(err)); // eslint-disable-line no-console
-  //   },
-  // },
 };
 </script>
 
--- src/store/store.js
+++ src/store/store.js
@@ -1,24 +1,16 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import axios from 'axios';
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
   strict: true,
+  
   state: {
-    todos: [
-      {
-        id: 0,
-        title: 'first',
-        completed: 'false',
-      },
-      {
-        id: 1,
-        title: 'second',
-        completed: 'true',
-      },
-    ],
+    todos: [],
   },
+  
   getters: {
     niceTodos: state => state.todos.map(todo => ({
       id: todo.id,
@@ -26,14 +18,50 @@ const store = new Vuex.Store({
       completed: todo.completed,
     })),
   },
+  
+  /* eslint-disable no-param-reassign */
   mutations: {
-    // addTodo: (newTodo) => {
-    //   this.$store.state.todos = [...this.$store.state.todos, newTodo];
-    // },
-    mutate: (state) => {
-      state.todos.forEach((todo) => {
-        todo.title += 'Mutata!'; // eslint-disable-line no-param-reassign
-      });
+    getTodos: (state, todoList) => {
+      state.todos = todoList;
+    },
+    addTodo: (state, newTodo) => {
+      state.todos = [...state.todos, newTodo];
+    },
+    deleteTodo: (state, filteredTodoList) => {
+      state.todos = filteredTodoList;
+    },
+  },
+  /* eslint-enable no-param-reassign */
+  
+  actions: {
+    getTodos: (context) => {
+      axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+        .then((res) => {
+          context.commit('getTodos', res.data);
+        })
+        .catch(err => console.error(err)); // eslint-disable-line no-console
+    },
+    
+    addTodo: (context, payload) => {
+      const { title, completed } = payload;
+      
+      axios.post('https://jsonplaceholder.typicode.com/todos', {
+        title,
+        completed,
+      })
+        .then((res) => {
+          context.commit('addTodo', res.data);
+        })
+        .catch(err => console.error(err)); // eslint-disable-line no-console
+    },
+    
+    deleteTodo: (context, id) => {
+      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        .then(() => {
+          const filteredTodoList = context.state.todos.filter(todo => todo.id !== id);
+          context.commit('deleteTodo', filteredTodoList);
+        })
+        .catch(err => console.error(err)); // eslint-disable-line no-console
     },
   },
 });
